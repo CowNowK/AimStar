265e05b7a1ff352a582f875eeb30323d954c8a21
CowNow <memz233li@foxmail.com>
2023-12-09 13:16:41 +0800

    revert offsets.cpp

--- CS2_External/Offsets.cpp
+++ CS2_External/Offsets.cpp
@@ -8,10 +8,11 @@ DWORD64 SearchOffsets(std::string Signature, DWORD64 ModuleAddress)
 
 	TempAddressList = ProcessMgr.SearchMemory(Signature, ModuleAddress, ModuleAddress + 0x4000000);
 
+	if (TempAddressList.size() <= 0)
+		return 0;
 
-	JZ(TempAddressList.size() <= 0);
-
-	JZ(!ProcessMgr.ReadMemory<DWORD>(TempAddressList.at(0) + 3, Offsets))
+	if (!ProcessMgr.ReadMemory<DWORD>(TempAddressList.at(0) + 3, Offsets))
+		return 0;
 
 	Address = TempAddressList.at(0) + Offsets + 7;
 	return Address;
@@ -20,59 +21,62 @@ DWORD64 SearchOffsets(std::string Signature, DWORD64 ModuleAddress)
 bool Offset::UpdateOffsets()
 {
 	DWORD64 ClientDLL = reinterpret_cast<DWORD64>(ProcessMgr.GetProcessModuleHandle("client.dll"));
-
-	JZ(ClientDLL);
+	if (ClientDLL == 0)
+		return false;
 
 	DWORD64 ServerDLL = reinterpret_cast<DWORD64>(ProcessMgr.GetProcessModuleHandle("server.dll"));
-
-	JZ(ServerDLL);
+	if (ClientDLL == 0)
+		return false;
 
 	DWORD64 TempAddress = 0;
 
 	TempAddress = SearchOffsets(Offset::Signatures::EntityList, ClientDLL);
-	JZ(TempAddress);
+	if (TempAddress == 0)
+		return false;
 
 	Offset::EntityList = TempAddress - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::LocalPlayerController, ClientDLL);
-	JZ(TempAddress);
+	if (TempAddress == 0)
+		return false;
 
 	Offset::LocalPlayerController = TempAddress - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::ViewMatrix, ClientDLL);
-	JZ(TempAddress);
+	if (TempAddress == 0)
+		return false;
 
 	Offset::Matrix = TempAddress - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::GlobalVars, ClientDLL);
-	JZ(TempAddress);
-
+	if (TempAddress == 0)
+		return false;
 
 	Offset::GlobalVars = TempAddress - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::ViewAngles, ClientDLL);
-	JZ(TempAddress);
-
-
-	JZ(!ProcessMgr.ReadMemory(TempAddress, TempAddress));
+	if (TempAddress == 0)
+		return false;
+	if (!ProcessMgr.ReadMemory(TempAddress, TempAddress))
+		return false;
 
 	Offset::ViewAngle = TempAddress + 0x6140 - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::LocalPlayerPawn, ClientDLL);
-	JZ(TempAddress);
-
+	if (TempAddress == 0)
+		return false;
 
 	Offset::LocalPlayerPawn = TempAddress + 0x138 - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::ForceJump, ClientDLL);
-	JZ(TempAddress);
-
+	if (TempAddress == 0)
+		return false;
 
 	Offset::ForceJump = TempAddress + 0x30 - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::PlantedC4, ClientDLL);
-	JZ(TempAddress);
-
+	if (TempAddress == 0)
+		return false;
 
 	Offset::PlantedC4 = TempAddress - ClientDLL;
 	return true;
