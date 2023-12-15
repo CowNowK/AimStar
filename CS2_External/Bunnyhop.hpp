fc2752770924bb0d755d7057816048764415a7b6
CowNow <memz233li@foxmail.com>
2023-12-15 23:02:21 +0800

    Fixed Bhop

--- CS2_External/Bunnyhop.hpp
+++ CS2_External/Bunnyhop.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "MenuConfig.hpp"
 #include "Entity.h"
+#include <thread>
+#include <chrono>
 
 namespace Bunnyhop
 {
@@ -22,6 +24,7 @@ namespace Bunnyhop
 
 		if (spacePressed && isInAir) // AirCheck = 1, is on ground
 		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(15));
 			gGame.SetForceJump(65537);
 		}
 		else if (spacePressed && !isInAir) // AirCheck = 0, isn't on ground
