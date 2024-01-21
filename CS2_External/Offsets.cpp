a61480aa96b25e9d762307146cd85b8fb95560f0
CowNow <memz233li@foxmail.com>
2024-01-21 15:35:22 +0800

    Fix dwViewAngles pattern and offset

--- CS2_External/Offsets.cpp
+++ CS2_External/Offsets.cpp
@@ -64,7 +64,7 @@ bool Offset::UpdateOffsets()
 	if (!ProcessMgr.ReadMemory(TempAddress, TempAddress))
 		return false;
 
-	Offset::ViewAngle = TempAddress + 0x6140 - ClientDLL;
+	Offset::ViewAngle = TempAddress + 0x8F30 - ClientDLL;
 
 	TempAddress = SearchOffsets(Offset::Signatures::LocalPlayerPawn, ClientDLL);
 	if (TempAddress == 0)
