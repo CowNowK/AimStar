a9f4a6d18c5ed2347375dfd4cbaba312c0d1f4d5
渟雲 <zuiun1337@gmail.com>
2024-02-14 17:19:14 +0800

    One-time switch

--- CS2_External/Features/GUI.h
+++ CS2_External/Features/GUI.h
@@ -438,10 +438,13 @@ namespace GUI
 						ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 10.f);
 						ImGui::TextColored(ImColor(255, 50, 0, 255), "This might cause BAN");
 					}
-					if (MenuConfig::Country == "CN" && MenuConfig::Language == 6)
+					if (MenuConfig::Country == "CN" && MenuConfig::Language == 6 && ESPConfig::winniethepool)
 					{
-						ESPConfig::winniethepool = true;
 						PutSwitch("Winnie", 10.f, ImGui::GetFrameHeight() * 1.7, &ESPConfig::winniethepool);
+					}
+					else
+					{
+						ESPConfig::winniethepool = false;
 					}
 
 
