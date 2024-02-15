e7c3e4dd7f72f427dc27161b09fbce32f08820a9
渟雲 <QuQ@outlook.it>
2024-02-16 02:35:44 +0800

    ROLLBACK

--- CS2_External/AimBot.hpp
+++ CS2_External/AimBot.hpp
@@ -21,7 +21,7 @@ namespace AimControl
     inline bool AutoShot = false;
     inline bool AimLock = false;
     inline float AimFov = 5;
-    inline float AimFovMin = .2f;
+    inline float AimFovMin = .5f;
     inline float Smooth = 2.0f;
     inline Vec2 RCSScale = { 1.f,1.f };
     inline std::vector<int> HotKeyList{ VK_LMENU, VK_LBUTTON, VK_RBUTTON, VK_XBUTTON1, VK_XBUTTON2, VK_CAPITAL, VK_LSHIFT, VK_LCONTROL };
