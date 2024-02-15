7cdc887925f48e4f615cae2b4165347514e33e47
渟雲 <QuQ@outlook.it>
2024-02-16 02:40:01 +0800

    DEAL CONFLICT

--- CS2_External/Features/GUI.h
+++ CS2_External/Features/GUI.h
@@ -320,8 +320,8 @@ namespace GUI
 					ImGui::Columns(2, nullptr, false);
 					ImGui::SetCursorPos(ImVec2(15.f, 24.f));
 					ImGui::SeparatorText(ICON_FA_USER" Aimbot");
-					float FovMin = 0.f, FovMax = 25.f, MinFovMax = 1.f;;
-					float SmoothMin = 0.f, SmoothMax = 5.f;
+					float FovMin = 0.f, FovMax = 25.f, MinFovMax = 1.f;
+					float SmoothMin = 0.4f, SmoothMax = 5.f;
 					PutSwitch(Lang::AimbotText.Enable, 10.f, ImGui::GetFrameHeight() * 1.7, &MenuConfig::AimBot);
 					if (MenuConfig::AimBot)
 					{
