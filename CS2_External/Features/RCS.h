13f2b23aaec62e04a2b820827b678526322b967c
渟雲 <QuQ@outlook.it>
2024-03-19 03:55:29 +0800

    try fix rcs bug

--- CS2_External/Features/RCS.h
+++ CS2_External/Features/RCS.h
@@ -68,18 +68,17 @@ namespace RCS
 	{
 		if (!MenuConfig::RCS)
 			return;
-
 		static Vec2 OldPunch;
-
 		if (LocalPlayer.Pawn.ShotsFired > RCSBullet)
-		{
+		{       
 			Vec2 viewAngles = LocalPlayer.Pawn.ViewAngle;
 			Vec2 delta = viewAngles - (viewAngles + (OldPunch - (LocalPlayer.Pawn.AimPunchAngle * 2.f)));
 
 			int MouseX = (int)(delta.y / (LocalPlayer.Client.Sensitivity * 0.011f) * RCSScale.x);
 			int MouseY = (int)(delta.x / (LocalPlayer.Client.Sensitivity * 0.011f) * RCSScale.y);
 
-			mouse_event(MOUSEEVENTF_MOVE, MouseX, -MouseY, 0, 0);
+			if ((GetAsyncKeyState(VK_LBUTTON) & 0x8000))
+				mouse_event(MOUSEEVENTF_MOVE, MouseX, -MouseY, 0, 0);
 
 			OldPunch = LocalPlayer.Pawn.AimPunchAngle * 2.0f;
 		}
