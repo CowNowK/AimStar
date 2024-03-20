246bbe61c045c8631e4d6b09139c070f85d44394
Mirkaan <98285577+Mirkaanks@users.noreply.github.com>
2024-03-20 08:36:54 -0300

    Update TriggerBot.cpp

--- CS2_External/TriggerBot.cpp
+++ CS2_External/TriggerBot.cpp
@@ -14,76 +14,84 @@ void TriggerBot::ReleaseMouseButton()
 
 void TriggerBot::Run(const CEntity& LocalEntity)
 {
-	if (!ProcessMgr.ReadMemory<DWORD>(LocalEntity.Pawn.Address + Offset::Pawn.iIDEntIndex, uHandle))
-		return;
-	if (uHandle == -1)
-		return;
+    DWORD uHandle;
+    DWORD64 ListEntry;
+    DWORD64 PawnAddress;
 
-	ListEntry = ProcessMgr.TraceAddress(gGame.GetEntityListAddress(), { 0x8 * (uHandle >> 9) + 0x10,0x0 });
-	if (ListEntry == 0)
-		return;
+    if (!ProcessMgr.ReadMemory<DWORD>(LocalEntity.Pawn.Address + Offset::Pawn.iIDEntIndex, uHandle) || uHandle == -1)
+        return;
 
-	if (!ProcessMgr.ReadMemory<DWORD64>(ListEntry + 0x78 * (uHandle & 0x1FF), PawnAddress))
-		return;
+    ListEntry = ProcessMgr.TraceAddress(gGame.GetEntityListAddress(), { 0x8 * (uHandle >> 9) + 0x10, 0x0 });
+    if (ListEntry == 0)
+        return;
 
-	if (!Entity.UpdatePawn(PawnAddress))
-		return;
+    if (!ProcessMgr.ReadMemory<DWORD64>(ListEntry + 0x78 * (uHandle & 0x1FF), PawnAddress))
+        return;
 
-	if (!IgnoreFlash && LocalEntity.Pawn.FlashDuration > 0.f)
-		return;
+    if (!Entity.UpdatePawn(PawnAddress))
+        return;
 
-	if (ScopeOnly)
-	{
-		bool isScoped;
-		ProcessMgr.ReadMemory<bool>(LocalEntity.Pawn.Address + Offset::Pawn.isScoped, isScoped);
-		if (!isScoped) {
-			return;
-		}
-	}
+    if (!IgnoreFlash && LocalEntity.Pawn.FlashDuration > 0.f)
+        return;
 
-	if (MenuConfig::TeamCheck)
-		AllowShoot = LocalEntity.Pawn.TeamID != Entity.Pawn.TeamID && Entity.Pawn.Health > 0;
-	else
-		AllowShoot = Entity.Pawn.Health > 0;
+    if (ScopeOnly)
+    {
+        bool isScoped;
+        ProcessMgr.ReadMemory<bool>(LocalEntity.Pawn.Address + Offset::Pawn.isScoped, isScoped);
+        if (!isScoped)
+            return;
+    }
 
-	if (!AllowShoot)
-		return;
+    bool AllowShoot;
+    if (MenuConfig::TeamCheck)
+        AllowShoot = LocalEntity.Pawn.TeamID != Entity.Pawn.TeamID && Entity.Pawn.Health > 0;
+    else
+        AllowShoot = Entity.Pawn.Health > 0;
 
-	static std::chrono::time_point LastTimePoint = std::chrono::steady_clock::now();
-	auto CurTimePoint = std::chrono::steady_clock::now();
-	if (CurTimePoint - LastTimePoint >= std::chrono::milliseconds(TriggerDelay))
-	{
-		const bool isAlreadyShooting = GetAsyncKeyState(VK_LBUTTON) < 0;
-		if (!isAlreadyShooting)
-		{
-			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-			std::thread TriggerThread(ReleaseMouseButton);
-			TriggerThread.detach();
-		}
-		LastTimePoint = CurTimePoint;
-	}
+    if (!AllowShoot)
+        return;
+
+    static std::chrono::time_point LastTimePoint = std::chrono::steady_clock::now();
+    auto CurTimePoint = std::chrono::steady_clock::now();
+    if (CurTimePoint - LastTimePoint >= std::chrono::milliseconds(TriggerDelay))
+    {
+        const bool isAlreadyShooting = GetAsyncKeyState(VK_LBUTTON) < 0;
+        if (!isAlreadyShooting)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            std::this_thread::sleep_for(std::chrono::milliseconds(FakeShotDelay));
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+        LastTimePoint = CurTimePoint;
+    }
 }
 
-void TriggerBot::TargetCheck(const CEntity& LocalEntity) noexcept
-{
-	if (!ProcessMgr.ReadMemory<DWORD>(LocalEntity.Pawn.Address + Offset::Pawn.iIDEntIndex, uHandle) || uHandle == -1)
-	{
-		CrosshairsCFG::isAim = false;
-	}
-	else
-	{
-		ListEntry = ProcessMgr.TraceAddress(gGame.GetEntityListAddress(), { 0x8 * (uHandle >> 9) + 0x10, 0x0 });
-		if (ListEntry != 0)
-		{
-			if (ProcessMgr.ReadMemory<DWORD64>(ListEntry + 0x78 * (uHandle & 0x1FF), PawnAddress))
-			{
-				if (Entity.UpdatePawn(PawnAddress))
-				{
-					CrosshairsCFG::isAim = CrosshairsCFG::TeamCheck ? (LocalEntity.Pawn.TeamID != Entity.Pawn.TeamID) : true;
-					return;
-				}
-			}
-		}
-		CrosshairsCFG::isAim = false;
-	}
+void TriggerBot::TargetCheck(const CEntity& LocalEntity) noexcept {
+    if (!ProcessMgr.ReadMemory<DWORD>(LocalEntity.Pawn.Address + Offset::Pawn.iIDEntIndex, uHandle) || uHandle == -1) {
+        CrosshairsCFG::isAim = false;
+        return;
+    }
+
+    const unsigned long long ENTITY_OFFSET = 0x78;
+    const unsigned long long ENTITY_INDEX_MASK = 0x1FF;
+
+    DWORD64 ListEntry = ProcessMgr.TraceAddress(gGame.GetEntityListAddress(), { 0x8 * (uHandle >> 9) + 0x10, 0x0 });
+    if (ListEntry == 0) {
+        CrosshairsCFG::isAim = false;
+        return;
+    }
+
+    DWORD64 PawnAddress;
+    const DWORD64 ENTITY_ADDRESS_OFFSET = ENTITY_OFFSET * (uHandle & ENTITY_INDEX_MASK);
+    if (!ProcessMgr.ReadMemory<DWORD64>(ListEntry + ENTITY_ADDRESS_OFFSET, PawnAddress)) {
+        CrosshairsCFG::isAim = false;
+        return;
+    }
+
+    if (!Entity.UpdatePawn(PawnAddress)) {
+        CrosshairsCFG::isAim = false;
+        return;
+    }
+
+    CrosshairsCFG::isAim = CrosshairsCFG::TeamCheck ? (LocalEntity.Pawn.TeamID != Entity.Pawn.TeamID) : true;
 }
