4fc67fbe8cdd9a51faca08e698548ff486a82558
渟雲 <zuiun1337@gmail.com>
2024-04-27 00:02:55 +0800

    Create offsets.hpp

---
+++ CS2_External/a2x/offsets.hpp
@@ -0,0 +1,51 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-04-26 04:15:57.943456300 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace offsets {
+        // Module: client.dll
+        namespace client_dll {
+            constexpr std::ptrdiff_t dwCSGOInput = 0x192EF60;
+            constexpr std::ptrdiff_t dwEntityList = 0x18C6268;
+            constexpr std::ptrdiff_t dwGameEntitySystem = 0x19E3B70;
+            constexpr std::ptrdiff_t dwGameEntitySystem_getHighestEntityIndex = 0x1510;
+            constexpr std::ptrdiff_t dwGameRules = 0x1923370;
+            constexpr std::ptrdiff_t dwGlobalVars = 0x172DD60;
+            constexpr std::ptrdiff_t dwGlowManager = 0x1923788;
+            constexpr std::ptrdiff_t dwLocalPlayerController = 0x1915C18;
+            constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x173A3C8;
+            constexpr std::ptrdiff_t dwPlantedC4 = 0x192C408;
+            constexpr std::ptrdiff_t dwPrediction = 0x173A290;
+            constexpr std::ptrdiff_t dwSensitivity = 0x19240B8;
+            constexpr std::ptrdiff_t dwSensitivity_sensitivity = 0x40;
+            constexpr std::ptrdiff_t dwViewAngles = 0x19342F0;
+            constexpr std::ptrdiff_t dwViewMatrix = 0x19278B0;
+            constexpr std::ptrdiff_t dwViewRender = 0x19280B0;
+            constexpr std::ptrdiff_t dwWeaponC4 = 0x18C9830;
+        }
+        // Module: engine2.dll
+        namespace engine2_dll {
+            constexpr std::ptrdiff_t dwBuildNumber = 0x516574;
+            constexpr std::ptrdiff_t dwNetworkGameClient = 0x515AC8;
+            constexpr std::ptrdiff_t dwNetworkGameClient_deltaTick = 0x258;
+            constexpr std::ptrdiff_t dwNetworkGameClient_getLocalPlayer = 0xF0;
+            constexpr std::ptrdiff_t dwNetworkGameClient_getMaxClients = 0x250;
+            constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x240;
+            constexpr std::ptrdiff_t dwWindowHeight = 0x5CEE4C;
+            constexpr std::ptrdiff_t dwWindowWidth = 0x5CEE48;
+        }
+        // Module: inputsystem.dll
+        namespace inputsystem_dll {
+            constexpr std::ptrdiff_t dwInputSystem = 0x367A0;
+        }
+        // Module: matchmaking.dll
+        namespace matchmaking_dll {
+            constexpr std::ptrdiff_t dwGameTypes = 0x1D21E0;
+            constexpr std::ptrdiff_t dwGameTypes_mapName = 0x1D2300;
+        }
+    }
+}
